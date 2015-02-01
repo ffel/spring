b4af46102d4035719730b94d4ead0c5232ef7d2b
Chris van Uffelen <vanuffelen@gmail.com>
2015-02-01 21:52:07 +0100

    better separation of system definition, runner, and method

--- spring.go
+++ spring.go
@@ -3,41 +3,46 @@ package main
 import "fmt"
 
 // see http://jordanorelli.com/post/42369331748/function-types-in-go-golang
+
+// num as short for float64
 type num float64
+
+// the spring is described by two first order ode
 type ode func([]num, num) num
 
+// euler and midpoint are integrators
+type integrator func([]num, num, num, []ode) []num
+
 func main() {
-	runner(euler)
-	runner(midpoint)
+	var k, m, b num
+
+	k = 1
+	m = 1
+	b = 1
+
+	dxdt := func(xx []num, t num) num { v := xx[1]; return v }
+	dvdt := func(xx []num, t num) num { x, v := xx[0], xx[1]; return -k*x/m - b*v/m }
+
+	odes := []ode{dxdt, dvdt}
+
+	fixed_step(euler, odes, -0.5, 0, 3, 0.25)
+	fixed_step(midpoint, odes, -0.5, 0, 3, 0.25)
 }
 
-func runner(method func([]num, num, num, []ode) []num) {
-	// k, m, b are system constants
-	// x, v will be calculated
-	// h is interval
-	// T is time
-	var k, m, b, x, v, h, T num
+func fixed_step(method integrator, dxdt []ode, x0, v0, tmax, h num) {
+	var x, v, T num
 
-	k = 1.0
-	m = 1.0
-	b = 1.0
+	x = x0
+	v = v0
 
-	x = -0.5
-	v = 0.0
-
-	h = 0.25
 	T = 0.0
 
-	f_x := func(x []num, t num) num { return x[1] }
-	f_v := func(x []num, t num) num { return -k*x[0]/m - b*x[1]/m }
-
 	x_n := []num{x, v}
-	f := []ode{f_x, f_v}
 
 	fmt.Printf("%9s %9s %9s %9s %9s\n", "t", "x", "v", "x'", "v'")
 
-	for T <= 3 {
-		kk := method(x_n, T, h, f)
+	for T <= tmax {
+		kk := method(x_n, T, h, dxdt)
 
 		fmt.Printf("%9.3f %9.2f %9.2f %9.2f %9.2f\n",
 			T, x_n[0], x_n[1], kk[0]/h, kk[1]/h)
